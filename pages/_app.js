4ba7062b50091f9312c135cf76a965d4119d3132
Gianluca Esposito <g@esposi.to>
2021-05-27 10:20:39 +0200

    Fix a11y issues

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
